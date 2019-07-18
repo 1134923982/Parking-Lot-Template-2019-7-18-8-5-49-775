d01db3e48a780668e357209618380dc12a64fd85
1134923982 <1134923982@qq.com>
2019-07-18 18:54:27 +0800

    [feat:create ParkingLot] [feat:init create V1__init.sql for create table parkingLot]

---
+++ src/main/java/com/thoughtworks/parking_lot/model/ParkingLot.java
@@ -0,0 +1,46 @@
+package com.thoughtworks.parking_lot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
+public class ParkingLot {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Min(value = 0)
+    private int capacity;
+    private String position;
+
+    public ParkingLot() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+drop table parking_lot if exists;
+
+drop sequence if exists hibernate_sequence ;
+create sequence hibernate_sequence start with 1 increment by 1 ;
+create table parking_lot
+(
+    id       bigint       not null,
+    capacity integer      not null check (capacity >= 0),
+    name     varchar(255) not null,
+    position varchar(255),
+    primary key (id)
+) ;
+alter table parking_lot
+    add constraint UK_PARKING_NAME unique (name);
