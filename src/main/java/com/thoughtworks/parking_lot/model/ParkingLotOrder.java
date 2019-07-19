a1ad0c7963e0df86535473298c30c6d7e93d672c
yu <1134923982@qq.com>
2019-07-19 23:59:04 +0800

    [story 2 AC1][feat][create entity class ParkingLotOrder.java]

---
+++ src/main/java/com/thoughtworks/parking_lot/model/ParkingLotOrder.java
@@ -0,0 +1,73 @@
+package com.thoughtworks.parking_lot.model;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class ParkingLotOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    private ParkingLot parkingLot;
+
+    private String carId;
+    private Date createTime;
+    private Date endTime;
+    @Column(columnDefinition = "boolean default false")
+    private boolean orderStatus;
+
+    public ParkingLotOrder() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public boolean isOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(boolean orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
